38e9bb35bd2ea8bbab408d07227444fd9501d9d8
Sahib <sahib.ptu@gmail.com>
2025-09-02 21:45:16 +0530

    Added home Route

--- app.js
+++ app.js
@@ -98,7 +98,6 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    console.log(req.user)
     res.locals.currentUser = req.user;
     
     next();
