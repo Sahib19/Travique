71519408eb40d417cb2a7631b1b2a63f919c8a91
Sahib <sahib.ptu@gmail.com>
2025-09-03 18:38:45 +0530

    UI Enhancement

--- init/index.js
+++ init/index.js
@@ -1,14 +1,16 @@
-//sending the sample data to the database
+// Load environment variables
+require("dotenv").config({ path: __dirname + "/../.env" });
 
 const mongoose = require("mongoose");
-const initData = require("./data.js");
+const initData = require("./data2.js");
 const Listing = require("../models/listing.js");
 
-
+// connecting with mongo
+const dbURL = process.env.ATLAS_MONGO_DB;
 // connecting with mongo
 async function main() {
     // below line will return a promise
-    await mongoose.connect('mongodb://127.0.0.1:27017/Wonderlust');
+    await mongoose.connect(dbURL);
 }
 
 main()
@@ -19,9 +21,13 @@ main()
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data =  initData.data.map((obj) => ({...obj , owner : "68ae02c6222496d25e9df7d4"}))
+    initData.data = initData.data.map((obj) => ({
+        ...obj,
+        owner: new mongoose.Types.ObjectId("68b6d45f97903e68d6c5e51c"),
+    }));
     await Listing.insertMany(initData.data);
-
-}
+    console.log("Database seeded successfully!");
+    mongoose.connection.close(); // close connection after seeding
+};
 
 initDB();
